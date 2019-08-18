f6550c44c7f7dce0d4463da4965020a820193dc5
Dylan Bowers <dylan.volibowers@gmail.com>
2019-08-17 22:15:53 -0400

    lots of styling

--- src/modules/Introduction.js
+++ src/modules/Introduction.js
@@ -1,18 +1,28 @@
 import React from 'react'
 import '../assets/css/Introduction.css'
 
-export default () => {
-    return (
-        <div id="intro-container">
-            <div id="name">Dylan Bowers</div>
-            <div id="slogan">Code&nbsp;
-                <span className="typeWriter" 
-                    data-speed="2.5"
-                    data-random="True"
-                    data-dltSpeed="False"
-                    data-text='["aesthetically.", "cleanly.", "with the future in mind."]'>
-                </span>
+class Introduction extends React.Component {
+    constructor(props) {
+        super(props);
+        this.nameRef = React.createRef();
+        this.sloganRef = React.createRef();
+    }
+
+    render() {
+        return (
+            <div id="intro-container">
+                <div ref={this.nameRef} class="text" id="name">Dylan Bowers</div>
+                <div ref={this.sloganRef} class="text" id="slogan">Code&nbsp;
+                    <span className="typeWriter" 
+                        data-speed="2.5"
+                        data-random="True"
+                        data-dltSpeed="False"
+                        data-text='["aesthetically.", "cleanly.", "with the future in mind."]'>
+                    </span>
+                </div>
             </div>
-        </div>
-    );
+        );
+    }
 }
+
+export default Introduction;
