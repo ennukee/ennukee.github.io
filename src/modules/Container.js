6fb3a1ee256f2763c04e799f5d8bf7f59ef59d43
Dylan Bowers <dylan.volibowers@gmail.com>
2021-09-24 15:11:21 -0400

    site rework

--- src/modules/Container.js
+++ src/modules/Container.js
@@ -1,95 +1,54 @@
-import React from 'react'
+import React, { useState } from 'react'
 import '../assets/css/Container.css'
-import Header from './Header';
-import Introduction from './Introduction';
 
-class Container extends React.Component {
-    constructor(props) {
-        super(props)
-        this.state = {
-            svgFill: '#222222',
-            introBgState: '#222222',
-            isScrolled: false,
-            svgDirection: '0',
-            svgFillOpacity: '1'
-        }
-    }
-
-    componentDidMount() {
-        window.addEventListener('scroll', this.handleScroll, true)
-    }
-
-    componentWillUnmount() {
-        window.removeEventListener('scroll', this.handleScroll)
-    }
-
-    /* why must they make svg opacity animations so difficult */
-    svgFillOpacityConversion = async (time, steps, start, end) => {
-        const gap = end - start;
-        const timePerStep = time / steps;
-        const gapPerStep = gap / steps;
-
-        console.log('DEBUG DATA', gap, steps,  timePerStep, gapPerStep);
-
-        this.setState({
-            svgFillOpacity: start
-        })
-        for ( let i = 0; i < steps; i++ ) {
-            // console.log(this.state.svgFillOpacity);
-            await this.setState({
-                svgFillOpacity: parseFloat(this.state.svgFillOpacity) + gapPerStep
-            })
-            await new Promise(r => setTimeout(r, timePerStep));
-        }
-        this.setState({
-            svgFillOpacity: end
-        })
-    }
-
-    handleScroll = async () => {
-        const Y = window.scrollY;
-        if ( Y > 0 && !this.state.isScrolled ) {
-            this.setState({
-                isScrolled: true,
-                introBgState: '#22222200'
-            });
-            await this.svgFillOpacityConversion(250, 10, 1, 0);
-            this.setState({
-                svgDirection: '320',
-                
-            });
-            this.svgFillOpacityConversion(250, 10, 0, 1);
-        }
-        else if ( Y === 0 && this.state.isScrolled ) {
-            this.setState({
-                isScrolled: false,
-                
-            });
-            await this.svgFillOpacityConversion(250, 10, 1, 0);
-            this.setState({
-                svgDirection: '0',
-                introBgState: '#222222'
-            });
-            this.svgFillOpacityConversion(250, 10, 0, 1);
-        }
-    }
-
-    render() {
-        return (
-            <div id="body">
-                <Header/>
-                <div id="top-container" style={{backgroundColor: this.state.introBgState}}>
-                    <Introduction/>
+function Container() {
+    const [ isLightMode, setLightMode ] = useState(false)
+    return (
+        <div
+            id="body"
+            className={isLightMode ? 'light' : 'dark'}
+        >   
+            <div id="background-color-fader" />
+            <div id="light-mode-controls">
+                <div
+                    className={`mode-select ${!isLightMode && 'selected'}`}
+                    id="dark-mode-select"
+                    onClick={() => setLightMode(false)}
+                />
+                <div
+                    className={`mode-select ${isLightMode && 'selected'}`}
+                    id="light-mode-select"
+                    onClick={() => setLightMode(true)}
+                />
+            </div>
+            <div id="intro-container">
+                <div class="text" id="name">ennukee<span id="period"></span></div>
+                <div class="text" id="pronunciation">en·nOO·kay</div>
+                <div class="text" id="slogan">code&nbsp;
+                    <span className="typeWriter" 
+                        data-speed="2.5"
+                        data-random="True"
+                        data-dltSpeed="False"
+                        data-text='["aesthetically", "cleanly", "with the future in mind"]'>
+                    </span>
                 </div>
-                <div id="wave-hr">
-                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
-                        <path fill={this.state.svgFill} fill-opacity={this.state.svgFillOpacity} 
-                        d={`M0,160L26.7,176C53.3,192,107,224,160,213.3C213.3,203,267,149,320,112C373.3,75,427,53,480,58.7C533.3,64,587,96,640,122.7C693.3,149,747,171,800,170.7C853.3,171,907,149,960,165.3C1013.3,181,1067,235,1120,234.7C1173.3,235,1227,181,1280,160C1333.3,139,1387,149,1413,154.7L1440,160L1440,${this.state.svgDirection}L1413.3,${this.state.svgDirection}C1386.7,${this.state.svgDirection},1333,${this.state.svgDirection},1280,${this.state.svgDirection}C1226.7,${this.state.svgDirection},1173,${this.state.svgDirection},1120,${this.state.svgDirection}C1066.7,${this.state.svgDirection},1013,${this.state.svgDirection},960,${this.state.svgDirection}C906.7,${this.state.svgDirection},853,${this.state.svgDirection},800,${this.state.svgDirection}C746.7,${this.state.svgDirection},693,${this.state.svgDirection},640,${this.state.svgDirection}C586.7,${this.state.svgDirection},533,${this.state.svgDirection},480,${this.state.svgDirection}C426.7,${this.state.svgDirection},373,${this.state.svgDirection},320,${this.state.svgDirection}C266.7,${this.state.svgDirection},213,${this.state.svgDirection},160,${this.state.svgDirection}C106.7,${this.state.svgDirection},53,${this.state.svgDirection},27,${this.state.svgDirection}L0,${this.state.svgDirection}Z`}></path>
-                    </svg>
+                <div id="socials">
+                    <a href="https://twitter.com/PriestismJP">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M24 4.557c-.883.392-1.832.656-2.828.775 1.017-.609 1.798-1.574 2.165-2.724-.951.564-2.005.974-3.127 1.195-.897-.957-2.178-1.555-3.594-1.555-3.179 0-5.515 2.966-4.797 6.045-4.091-.205-7.719-2.165-10.148-5.144-1.29 2.213-.669 5.108 1.523 6.574-.806-.026-1.566-.247-2.229-.616-.054 2.281 1.581 4.415 3.949 4.89-.693.188-1.452.232-2.224.084.626 1.956 2.444 3.379 4.6 3.419-2.07 1.623-4.678 2.348-7.29 2.04 2.179 1.397 4.768 2.212 7.548 2.212 9.142 0 14.307-7.721 13.995-14.646.962-.695 1.797-1.562 2.457-2.549z"/></svg>
+                    </a>
+                    <a href="https://github.com/ennukee">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M12 0c-6.626 0-12 5.373-12 12 0 5.302 3.438 9.8 8.207 11.387.599.111.793-.261.793-.577v-2.234c-3.338.726-4.033-1.416-4.033-1.416-.546-1.387-1.333-1.756-1.333-1.756-1.089-.745.083-.729.083-.729 1.205.084 1.839 1.237 1.839 1.237 1.07 1.834 2.807 1.304 3.492.997.107-.775.418-1.305.762-1.604-2.665-.305-5.467-1.334-5.467-5.931 0-1.311.469-2.381 1.236-3.221-.124-.303-.535-1.524.117-3.176 0 0 1.008-.322 3.301 1.23.957-.266 1.983-.399 3.003-.404 1.02.005 2.047.138 3.006.404 2.291-1.552 3.297-1.23 3.297-1.23.653 1.653.242 2.874.118 3.176.77.84 1.235 1.911 1.235 3.221 0 4.609-2.807 5.624-5.479 5.921.43.372.823 1.102.823 2.222v3.293c0 .319.192.694.801.576 4.765-1.589 8.199-6.086 8.199-11.386 0-6.627-5.373-12-12-12z"/></svg>
+                    </a>
+                    <a href="https://www.linkedin.com/in/dylan-bowers/">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M19 0h-14c-2.761 0-5 2.239-5 5v14c0 2.761 2.239 5 5 5h14c2.762 0 5-2.239 5-5v-14c0-2.761-2.238-5-5-5zm-11 19h-3v-11h3v11zm-1.5-12.268c-.966 0-1.75-.79-1.75-1.764s.784-1.764 1.75-1.764 1.75.79 1.75 1.764-.783 1.764-1.75 1.764zm13.5 12.268h-3v-5.604c0-3.368-4-3.113-4 0v5.604h-3v-11h3v1.765c1.396-2.586 7-2.777 7 2.476v6.759z"/></svg>
+                    </a>
+                    <a href="mailto:dylanvolibowers@gmail.com">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M12 12.713l-11.985-9.713h23.971l-11.986 9.713zm-5.425-1.822l-6.575-5.329v12.501l6.575-7.172zm10.85 0l6.575 7.172v-12.501l-6.575 5.329zm-1.557 1.261l-3.868 3.135-3.868-3.135-8.11 8.848h23.956l-8.11-8.848z"/></svg>
+                    </a>
                 </div>
             </div>
-        );
-    }
+        </div>
+    );
 }
 
 export default Container;
